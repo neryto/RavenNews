e25b806d558e32aae89aa79da8e508005c79b5d4
Nery Bustos <nery.bustos@bnext.es>
2024-03-07 17:20:20 -0600

    - Add database module shared between list and detail features - Add domain into detail module

--- feature/list/src/main/java/com/raven/home/di/NewsModule.kt
+++ feature/list/src/main/java/com/raven/home/di/NewsModule.kt
@@ -1,13 +1,9 @@
 package com.raven.home.di
 
-import android.content.Context
-import androidx.room.Room
-import com.raven.database.NewsDataBase
 import com.raven.home.data.remote.interactor.NetworkInteractor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
