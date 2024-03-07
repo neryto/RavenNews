f48a649e61d78a522262c3d672a6a5845f28b44a
Nery Bustos <nery.bustos@bnext.es>
2024-03-06 22:04:57 -0600

    - Implement room into the project - Save data when fetch it from server - Load data from db when an error was happen

--- feature/home/src/main/java/com/raven/home/domain/NewsLocalDataSource.kt
+++ feature/home/src/main/java/com/raven/home/domain/NewsLocalDataSource.kt
@@ -1,7 +1,6 @@
 package com.raven.home.domain
 
 import com.raven.home.data.local.entity.NewsEntity
-import kotlinx.coroutines.flow.Flow
 
 interface NewsLocalDataSource {
     suspend fun getNews(): List<NewsEntity>?
